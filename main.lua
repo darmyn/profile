b866e2da7dcb8774d662f36188ce5833659e5896
darmyn <81786165+darmyn@users.noreply.github.com>
2022-11-12 19:44:09 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,120 @@
+local dataStoreService = game:GetService("DataStoreService")
+local players = game:GetService("Players")
+
+local template = require(script.template)
+
+local autoSaving = false
+local profile = {} :: profile
+profile.prefix = "profile"
+profile.retries = 3
+profile.active = {
+	amount = 0, 
+	profiles = {} :: {[Player]: profile}
+}
+
+local function standardLoad(dataStore: DataStore, key: string): template.type
+	for _ = 0, profile.retries do
+		local success, result = pcall(dataStore.GetAsync, dataStore, key)
+		if success then
+			return result
+		else
+			warn(result)
+			continue
+		end
+	end
+end
+
+function profile.new(player: Player, loader: (DataStore, string) -> (template)?)
+	local self = setmetatable({}, {__index = profile})
+	self.player = player
+	self.key = ("%s_%d"):format(self.prefix, self.player.UserId)
+	self.dataStore = dataStoreService:GetDataStore("profiles", self.key)
+	self.data = (loader) and loader(self.dataStore, self.key) or standardLoad(self.dataStore, self.key) :: template
+	self.shouldAutoSave = true
+	profile.active.amount += 1
+	profile.active.profiles[player] = self
+	return self
+end
+
+--[[
+
+    This function could improve. Could have the provided duration scale based on the amount of players and what is reasonable for the system
+        to not go over it's rate limits.
+
+]]
+
+function profile.autoSave(duration: number, cooldown: number)
+	if not autoSaving then
+		autoSaving = true
+		while autoSaving do
+			for _, currentProfile: profile in pairs(profile.active) do
+				if currentProfile.shouldAutoSave then
+					task.spawn(currentProfile.save, currentProfile)
+					task.wait(duration / profile.active.amount)
+				end
+			end
+			task.wait(cooldown or 0)
+		end
+	end
+	return {
+		cancel = function()
+			autoSaving = false
+		end
+	}
+end
+
+function profile.autoDisconnect()
+	players.PlayerRemoving:Connect(function(player: Player)
+		local selectedProfile = profile.active.profiles[player]
+		if selectedProfile then
+			selectedProfile:destroy()
+		end
+	end)
+end
+
+type reconcileRecursiveMemory = {
+	dataDimension: {},
+	templateDimension: {}
+}
+
+function profile:reconcile(_recursiveMemory: reconcileRecursiveMemory?)
+	local dataDimension = (_recursiveMemory) and _recursiveMemory.dataDimension or self.data
+	local templateDimension = (_recursiveMemory) and _recursiveMemory.templateDimension or template()
+	for k, v in pairs(templateDimension) do
+		if typeof(v) == "table" then
+			local newDataDimension = dataDimension[k] or {}
+			profile:reconcile({
+				dataDimension = newDataDimension,
+				templateDimension = v
+			})
+		elseif not dataDimension[k] then
+			dataDimension[k] = v
+		end
+	end
+end
+
+function profile:save()
+	for _ = 0, self.retries do
+		local success, result = pcall(self.dataStore.SetAsync, self.dataStore, self.key, self.data)
+		if success then
+			return
+		else
+			warn(result)
+			continue
+		end
+	end
+end
+
+function profile:destroy()
+	self:save()
+	profile.active.amount -= 1
+	profile.active.profiles[self.player] = nil
+	setmetatable(self, nil)
+	table.clear(self)
+end
+
+type template = template.type
+type profile = typeof(profile) & typeof(profile.new(Instance.new("Player")))
+export type type = profile
+
+return profile
